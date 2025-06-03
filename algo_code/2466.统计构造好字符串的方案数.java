0abe8dc7fa6aa172be9b46179c940801ebbea748
quan7u <quan7u@qq.com>
2025-06-03 12:49:45 +0800

    爬楼梯变形题

--- algo_code/2466.统计构造好字符串的方案数.java
+++ algo_code/2466.统计构造好字符串的方案数.java
@@ -58,8 +58,23 @@
 
 // @lc code=start
 class Solution {
+    int MOD = 1_000_000_007;
     public int countGoodStrings(int low, int high, int zero, int one) {
-        
+        int res = 0;
+        int[] dp = new int[high + 1]; // 代表长度恰好为i的组合数
+        dp[0] = 1;
+        for (int i = 1; i <= high; i++) {
+            if (i >= zero) {
+                dp[i] = dp[i - zero];
+            }
+            if (i >= one) {
+                dp[i] =(dp[i] + dp[i - one]) % MOD;
+            }
+            if (i >= low) {
+                res = (res + dp[i]) % MOD;
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
