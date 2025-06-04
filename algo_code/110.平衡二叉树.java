717453047db3ee6a772a51cba1936d1e18c3277e
quan7u <quan7u@qq.com>
2025-06-05 00:28:07 +0800

    二叉树的递归和回溯

---
+++ algo_code/110.平衡二叉树.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=java
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode.cn/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.40%)
+ * Likes:    1611
+ * Dislikes: 0
+ * Total Accepted:    716.7K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是 平衡二叉树  
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 <= Node.val <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * 平衡二叉树 是指该树所有节点的左右子树的高度相差不超过 1。
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) >= 0;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (Math.abs(left - right) > 1 || left < 0 || right < 0) {
+            return -1; // 高度是正数，使用负数提早结束递归
+        }
+        return Math.max(left, right) + 1;
+    }
+}
+// @lc code=end
+
---
+++ algo_code/113.路径总和-ii.java
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=java
+ *
+ * [113] 路径总和 II
+ *
+ * https://leetcode.cn/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (63.90%)
+ * Likes:    1188
+ * Dislikes: 0
+ * Total Accepted:    470K
+ * Total Submissions: 735.4K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ * 
+ * 叶子节点 是指没有子节点的节点。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+ * 输出：[[5,4,11,2],[5,8,4,5]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [1,2,3], targetSum = 5
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1,2], targetSum = 0
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点总数在范围 [0, 5000] 内
+ * -1000 
+ * -1000 
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        dfs(root, targetSum, res, new ArrayList<>());
+        return res;
+    }
+
+    private void dfs(TreeNode root, int targetSum, List<List<Integer>> res, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            res.add(new ArrayList<>(list));
+        } else {
+            targetSum -= root.val;
+            dfs(root.left, targetSum, res, list);
+            dfs(root.right, targetSum, res, list);
+        }
+        list.remove(list.size() - 1);
+        
+    }
+}
+// @lc code=end
+
