7196acfa1ebf379e62f1e4642e3cf01902fe502f
quan7u <quan7u@qq.com>
2025-06-16 00:02:23 +0800

    数组题

---
+++ algo_code/41.缺失的第一个正数.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=41 lang=java
+ *
+ * [41] 缺失的第一个正数
+ *
+ * https://leetcode.cn/problems/first-missing-positive/description/
+ *
+ * algorithms
+ * Hard (47.21%)
+ * Likes:    2368
+ * Dislikes: 0
+ * Total Accepted:    558.1K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,2,0]'
+ *
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * 解释：范围 [1,2] 中的数字都在数组中。
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * 解释：1 在数组中，但 2 没有。
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ * 解释：最小的正数 1 没有出现。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * 找出其中没有出现的最小的正整数
+ * 1、O(n)：提示不能够排序
+ * 2、只使用常数级别额外空间的解决方案：原地
+ * 
+ * 预期效果：nums[0] = 1;nums[1] = 2
+ * 置换的核心：
+ * 1、[1, n] 边界外数据访问存在越界
+ * 2、若两个位置的元素相等，跳过（避免死循环）
+ * 
+ * 最后遍历若没有找到缺失的元素，返回n+1
+ * 
+ * TC:O(n) 元素若被交换到正确的位置，不会重复处理
+ * SC:O(1)
+ */
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int t = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = t;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
+// @lc code=end
+
